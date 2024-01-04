be102f963958e87005888a9dcc91af5e6ea5a7cd
GitJesm <kemnb@foxmail.com>
2024-01-04 17:13:18 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = np.array([-1.0, -2.0, 1.0])
+y = x > 0
+
+# def step_function(x):
+#     y = x > 0
+#     return y.astype(np.int)
+#
+#
+# res = step_function(x=x)
+print("~~~~")
+print(y)
